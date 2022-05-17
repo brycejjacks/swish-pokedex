c0aa38028e62539269ace1c9a660b83577b398ae
brycejjacks <brycejjacks@gmail.com>
2022-05-17 05:13:52 -0700

    ref(pokemonSchema): update data

--- src/server/schemas/pokemon.schema.js
+++ src/server/schemas/pokemon.schema.js
@@ -4,7 +4,10 @@ const pokemonSchema = new mongoose.Schema({
     id: {type: Number, required: true},
     name: {type: String, required: true},
     image: {type: String, required: true},
-    type: {type: String, required: true}
+    types: [{type: String, required: true}],
+    abilities: [{type: String, required: true}],
+    flavor_text: {type: String, required: true},
+    stats:[{type: mongoose.SchemaTypes.Mixed, required: true}]
 })
 
-export const PokemonModel = mongoose.model('pokemon', pokemonSchema);
+export const PokemonModel = mongoose.model('Pokemon', pokemonSchema);

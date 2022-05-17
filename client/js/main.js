01b817ce262968b4b608a69fdad1ef180914d898
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2022-05-17 14:41:36 -0700

    (feat: Added log, footer, and input bar)

--- client/js/main.js
+++ client/js/main.js
@@ -6,9 +6,10 @@ fetch('http://localhost:3000/api/pokedex').then(res => res.json()).then((pokeLis
         map(
             (pokedude) => `
         <li class="card">
+             <div class="card-number">${pokedude.id}</div>
             <img class="card-image" src="${pokedude.image}"/>
-            <h2 class="card-title">${pokedude.id}. ${pokedude.name}</h2>
-            <p class="card-subtitle">Type: ${pokedude.type}</p>
+            <h2 class="card-title"> ${pokedude.name}</h2>
+           
         </li>
     `
         )
@@ -20,18 +21,18 @@ fetch('http://localhost:3000/api/pokedex').then(res => res.json()).then((pokeLis
 
 
 
-const displayPokemon = (pokemon) => {
-    console.log(pokemon);
-    const pokemonHTMLString = pokemon
-        .map(
-            (pokedudes) => `
-        <li class="card">
-            <img class="card-image" src="${pokedudes.image}"/>
-            <h2 class="card-title">${pokedudes.id}. ${pokedudes.name}</h2>
-            <p class="card-subtitle">Type: ${pokedudes.type}</p>
-        </li>
-    `
-        )
-        .join('');
-    pokedex.innerHTML = pokemonHTMLString;
-};
+// const displayPokemon = (pokemon) => {
+//     console.log(pokemon);
+//     const pokemonHTMLString = pokemon
+//         .map(
+//             (pokedudes) => `
+//         <li class="card">
+//             <img class="card-image" src="${pokedudes.image}"/>
+//             <h2 class="card-title"> ${pokedudes.name}</h2>
+//             <p class="card-subtitle">Type: ${pokedudes.type}</p>
+//         </li>
+//     `
+//         )
+//         .join('');
+//     pokedex.innerHTML = pokemonHTMLString;
+// };

aaa099ab93359c03fa8ff4109da70b5c9791346c
Yadeta Getachew <86324546+yadetagetachew@users.noreply.github.com>
2022-05-24 17:19:35 -0700

    style: added animation to cards and icons for types

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,11 +1,22 @@
 const typeImg = {
     water: "./media/Pokemon_Type_Icon_Water.png",
-    fire:"./media/Pokémon_Fire_Type_Icon 2.png",
-    bug:"./",
-    dark:"",
+    fire:"./media/Pokemon_Type_Icon_Fire.png",
+    bug:"./media/Pokemon_Type_Icon_Bug.png",
+    dark:"./media/Pokemon_Type_Icon_Dark.png",
     grass:"./media/Pokemon_Type_Icon_Grass.png",
     poison:"./media/Pokemon_Type_Icon_Poison.png",
-
+    fighting:"./media/Pokemon_Type_Icon_Fighting.png",
+    fairy:"./media/Pokemon_Type_Icon_Fairy.png",
+    electric:"./media/Pokemon_Type_Icon_Electric.png",
+    flying:"./media/Pokemon_Type_Icon_Flying.png",
+    ghost:"./media/Pokemon_Type_Icon_Ghost.png",
+    ground:"./media/Pokemon_Type_Icon_Ground.png",
+    ice:"./media/Pokemon_Type_Icon_Ice.png",
+    normal:"./media/Pokemon_Type_Icon_Normal.png",
+    psychic:"./media/Pokemon_Type_Icon_Psychic.png",
+    steel:"./media/Pokemon_Type_Icon_Steel.png",
+    rock:"./media/Pokemon_Type_Icon_Rock.png",
+    dragon:"./media/Pokemon_Type_Icon_Dragon.png",
 }
 
 
@@ -53,7 +64,7 @@ const userPokemonList = [
     name: "coolGuy",
     image:
       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/150.png",
-    types: ["grass", "physic"],
+    types: ["grass", "psychic"],
     abilities: ["overgrow", "chlorophyll"],
     flavor_text:
       "A strange seed was planted on its back at birth. The plant sprouts and grows with this POKéMON.",
@@ -84,16 +95,13 @@ fetch("http://localhost:3000/api/pokedex")
     const pokemonHTMLString = pokeList
       .map(
         (pokedude) => `
-        <li class="card" onclick="updateInventory()">
+        <li class="card" id="triggerButton" data-bs-toggle="modal" data-bs-target="#exampleModal" onclick="updateInventory()">
             <div class="card-number">${pokedude.id}</div>
             <img class="card-image" src="${pokedude.image}"/>
             <h2 class="card-title"> ${pokedude.name}</h2>
             <p class="card-subtitle">
                 ${mapTypes(pokedude.types)}
             </p>
-            <button type="button" class="btn btn-primary" id="triggerButton" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-            </button>
         </li>
     `
       )
@@ -159,7 +167,7 @@ function getUserPokemonList() {
 
     userPokemonList.forEach((userPokemon) => {
         const userPokemonLI = document.createElement('li');
-        userPokemonLI.classList.add('card', 'userPokemonLI');
+        userPokemonLI.classList.add('cardSide', 'userPokemonLI');
         userPokemonLI.innerHTML = userPokemon.name;
         
         const userPokemonLIImage = document.createElement('img');
